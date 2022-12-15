8f9766c97d0954ba0d286bf75c4fa25edb8ccf3d
jeff <geoffreyerastus956@gmail.com>
2022-12-14 19:31:59 -0500

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+var navbar = document.getElementById("navbar");
+var menu = document.getElementById("menu");
+ window.onscroll = function(){
+     if(window.pageYOffset >= menu.offsetTop){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+      }
+ }
